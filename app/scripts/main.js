3737f979ca45065e0ac502e3532788b43ca9e9be
Stephen Wright <mario.luigi.star@gmail.com>
2014-03-21 10:19:21 -0400

    adding marionette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,16 +3,31 @@ require.config({
         jquery: '../bower_components/jquery/jquery'
 ,
         bootstrap: 'vendor/bootstrap/bootstrap'
+
+	    ,backbone: "../bower_components/backbone/backbone"
+	    ,marionette: "../bower_components/marionette/lib/core/amd/backbone.marionette.min"
+	    ,underscore: "../bower_components/underscore/underscore"
+	    ,'backbone.babysitter':'../bower_components/backbone.babysitter/lib/amd/backbone.babysitter.min'
+			,'backbone.wreqr':'../bower_components/backbone.wreqr/lib/amd/backbone.wreqr.min'
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
-        }
+        },
+		    "backbone": {
+			    "deps": ["underscore", "jquery"],
+			    "exports": "Backbone"
+		    },
+		    //Marionette
+		    "marionette": {
+			    "deps": ["underscore", "backbone", "jquery",'backbone.wreqr','backbone.babysitter'],
+			    "exports": "Marionette"
+		    }
     }
 });
 
-require(['app', 'jquery', 'bootstrap','localforage'], function (app, $) {
+require(['app', 'jquery', 'bootstrap','localforage', 'backbone','marionette'], function (app, $) {
     'use strict';
     // use app here
 		app();
